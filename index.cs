bfe39af9229102b2ce9d406425950bfb0e82d00d
NilaMony2001 <nila108335@gmail.com>
2023-12-04 23:07:07 +0600

    class work

---
+++ index.cs
@@ -0,0 +1,46 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+}
+
+.container {
+    max-width: 800px;
+    margin: 50px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    text-align: center;
+    color: #333;
+}
+
+input {
+    padding: 10px;
+    width: 70%;
+}
+
+button {
+    padding: 10px;
+    cursor: pointer;
+}
+
+#resultContainer {
+    margin-top: 20px;
+}
+
+.mealStyle {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+}
+
+img {
+    max-width: 100%;
+    height: auto;
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+function connect() {
+  var searchTerm = document.getElementById("searchBox").value;
+  var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+  console.log(url);
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => display(data));
+}
+
+function display(data) {
+  var allMeals = data.meals;
+  console.log(allMeals);
+
+  var oldContent = document.getElementById("container");
+  oldContent.textContent = "";
+
+  for (var i = 0; i < allMeals.length; i++) {
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = `Meal Name: ${allMeals[i].strMeal}  <br>
+                         Meal ID: ${allMeals[i].idMeal}<br>
+                         <img src="${allMeals[i].strMealThumb}"> <br> <br>
+                         Cooking Instructions: <p>
+                         ${allMeals[i].strInstructions}  </p> <br>;
+                         Ingredient 7: ${allMeals[i].strIngredient7} <br></br>`;
+
+    newDiv.classList.add("mealStyle");
+    oldContent.appendChild(newDiv);
+  }
+}
+
